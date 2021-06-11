bd0cbd60d031206e4893a0ab4841ba707d76adde
citrix <citrix@soulerys-Mac-mini.local>
2021-06-11 10:39:37 +0800

    learn collection view layout

--- learniOS/VideoViewController.m
+++ learniOS/VideoViewController.m
@@ -28,6 +28,9 @@
     self.view.backgroundColor = [UIColor whiteColor];
     
     UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
+    flowLayout.minimumLineSpacing = 10;
+    flowLayout.minimumInteritemSpacing = 10;
+    //flowLayout.itemSize = CGSizeMake((self.view.frame.size.width - 10) / 2, 300);
     
     UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:flowLayout];
     collectionView.delegate = self;
@@ -39,7 +42,7 @@
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return 100;
+    return 20;
 }
 
 // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
@@ -47,6 +50,14 @@
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cellId" forIndexPath:indexPath];
     cell.backgroundColor = [UIColor grayColor];
     return cell;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.item % 3 == 0) {
+        return CGSizeMake(self.view.frame.size.width, 300);
+    } else {
+        return CGSizeMake((self.view.frame.size.width - 10) / 2, 300);
+    }
 }
 
 @end
