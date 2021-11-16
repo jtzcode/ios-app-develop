757e6a03b00ec812effbb013458635bb8c9837d2
citrix <citrix@soulerys-mini.citrite.net>
2021-11-16 18:42:15 +0800

    learn progress view

--- learniOS/DetailedViewController.m
+++ learniOS/DetailedViewController.m
@@ -10,10 +10,14 @@
 
 @interface DetailedViewController () <WKNavigationDelegate>
 @property (nonatomic, strong, readwrite) WKWebView *webView;
+@property (nonatomic, strong, readwrite) UIProgressView *progressView;
 @end
 
 @implementation DetailedViewController
 
+- (void)dealloc {
+    [self.webView removeObserver:self forKeyPath:@"estimatedProgress"];
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
@@ -22,13 +26,25 @@
         self.webView.navigationDelegate = self;
         self.webView;
     })];
+    	
+    [self.view addSubview:({
+            self.progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 88, self.view.frame.size.width, 20)];
+            self.progressView;
+    })];
     
     [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.citrix.com"]]];
+    
+    [self.webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
 }
 
 - (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
     NSLog(@"Prepare to load URL: %@", navigationAction.request.URL.absoluteString);
     decisionHandler(WKNavigationActionPolicyAllow);
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    NSLog(@"Loading progress: %f", self.webView.estimatedProgress);
+    self.progressView.progress = self.webView.estimatedProgress;
 }
 
 @end
